653f4663224c5654915126819110461475259df5
Erik Jacobsen <slowerthanyou@gmail.com>
2017-03-10 14:10:08 -0800

    mode 2 done

--- main.js
+++ main.js
@@ -14,9 +14,13 @@ $(document).ready(function(){
     var value2 = $("#value2").val()
     $("#answer").val(Number(value1) / Number(value2))
   });
-    $("#multiply").on("click", function(){
+  $("#multiply").on("click", function(){
     var value1 = $("#value1").val()
     var value2 = $("#value2").val()
     $("#answer").val(Number(value1) * Number(value2))
   });
+    $("#clear").on("click", function(){
+      $("#answer").val("")
+    });
+
 })
